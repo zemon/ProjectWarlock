ee0b50e9b7bc33490be94546e02b78e4ead23344
MikkelDjurhuus <thekookaburraz@gmail.com>
2014-11-24 20:25:21 +0100

    ...
    ...

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,7 +7,10 @@ public class GameManager : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-        Invoke("SetupTeams", 1);
+        if (Network.isServer)
+        {
+            networkView.RPC("SetupTeams", RPCMode.All);
+        }
 	}
 	
 	// Update is called once per frame
@@ -20,31 +23,31 @@ public class GameManager : MonoBehaviour {
 
     }
 
+    [RPC]
     private void SetupTeams()
     {
+        Debug.Log("TEsting setup");
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
-        if (freeForAll)
+
+        for (int i = 0; i < players.Length; i++)
         {
-            for (int i = 0; i < players.Length; i++)
+            players[i].GetComponent<PlayerSpells>().team = i;
+            switch (i)
             {
-                players[i].GetComponent<PlayerSpells>().team = i;
-                switch (i)
-                {
-                    case 0:
-                        players[i].renderer.material.color = Color.green;
-                        break;
-                    case 1:
-                        players[i].renderer.material.color = Color.blue;
-                        break;
-                    case 2:
-                        players[i].renderer.material.color = Color.red;
-                        break;
-                    case 3:
-                        players[i].renderer.material.color = Color.yellow;
-                        break;
-                    default:
-                        break;
-                }
+                case 0:
+                    players[i].renderer.material.color = Color.green;
+                    break;
+                case 1:
+                    players[i].renderer.material.color = Color.blue;
+                    break;
+                case 2:
+                    players[i].renderer.material.color = Color.red;
+                    break;
+                case 3:
+                    players[i].renderer.material.color = Color.yellow;
+                    break;
+                default:
+                    break;
             }
         }
     }
